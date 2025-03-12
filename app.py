d31a3fb9eb4aae0e4b049a37414fbee039c9d3fa
unknown <osaidfarooqui@outlook.com>
2025-03-12 15:31:43 +0500

    ChAnGeS

--- app.py
+++ app.py
@@ -9,25 +9,34 @@ st.set_page_config(
     page_icon=":1234:",
     layout="centered"
 )
+    
+if "password" not in st.session_state:
+    st.session_state.password = ""
 
 def app():
-    st.header(':sparkles: Password Generator _by :red[Osaid]_')
-    st.markdown('''#### Inspired by [LastPass Password Generator.](https://www.lastpass.com/features/password-generator) :1234: &mdash;\
-                
+    st.header(':sparkles: Password Generator & Strength Checker _by :red[Osaid]_')
+    st.subheader("Test your own password:")
+    user_input = st.text_input("Test your own password:", value=st.session_state.password, label_visibility= "hidden")
+    if user_input:
+        if user_input != st.session_state.password:
+            st.session_state.password = user_input
 
-Creates a strong, unique passwords with customizable options, including length, special characters, and numbers.🎯 
-It ensures randomness for maximum security against hacking attempts🎉🚀.  
-''')
+    hr = st.container()
+    left, center, right = hr.columns([3,1,3])
+    left.divider()
+    right.divider()
+    center.markdown("<h3 style='display: flex; justify-content: center; align-tems: center; padding-bottom:0px'>OR</h3>", unsafe_allow_html=True)
+    st.subheader("Generate a password:")
     begin = st.container()
     column1, column2 = begin.columns([3, 1])
     butt1, butt2, butt3 = column2.columns(3)
     my_bar = begin.progress(0)
 
     col1, col2, col3 = st.columns(3)
-    length = 8
+    length = len(st.session_state.password)
     with col1:
         length = st.slider(
-        "Password Length:", 1, 15, value = length
+        "Password Length:", 1, 15, value = length,
         )
         st.text(length)
     with col2:
@@ -74,6 +83,7 @@ It ensures randomness for maximum security against hacking attempts🎉🚀.
     population = []
     if 'passwords' not in st.session_state:
         st.session_state.passwords = []
+
     if uppercase == True:
         population.append(string.ascii_uppercase)
     if lowercase == True:
@@ -87,15 +97,14 @@ It ensures randomness for maximum security against hacking attempts🎉🚀.
         single_string = "".join(population)
         def gen_pass(): 
             password = ''.join(random.choices( single_string ,k=length))
-            return password
-        password = gen_pass()
+            st.session_state.password = password
         def progress_bar():
             score = 0
-            length = len(password)
-            upper_case = any(c.isupper() for c in password)
-            lower_case = any(c.islower() for c in password)
-            special = any(c in string.punctuation for c in password)
-            digits = any(c.isdigit() for c in password)
+            length = len(st.session_state.password)
+            upper_case = any(c.isupper() for c in st.session_state.password)
+            lower_case = any(c.islower() for c in st.session_state.password)
+            special = any(c in string.punctuation for c in st.session_state.password)
+            digits = any(c.isdigit() for c in st.session_state.password)
 
             characters = [upper_case, lower_case, special, digits]
 
@@ -111,26 +120,15 @@ It ensures randomness for maximum security against hacking attempts🎉🚀.
             score += sum(characters) - 1
 
             if score < 4:
-                my_bar.progress(30, text='Weak Password!')
+                my_bar.progress(30, text=f'Weak Password! Score: {score}')
             elif score == 4:
-                my_bar.progress(50, text='Medium Password!')
+                my_bar.progress(50, text=f'Medium Password! Score: {score}')
             elif 4 < score < 6:
-                my_bar.progress(80, text='Strong Password!')
+                my_bar.progress(80, text=f'Strong Password! Score: {score}')
             else:
-                my_bar.progress(100, text='Very Strong Password!')
-            
-            # if len(password) > 11:
-            #     my_bar.progress(100, text='Impossible to crack!')
-            # elif len(password) > 8:
-            #     my_bar.progress(80, text='Very Strong!')
-            # elif len(password) > 6:
-            #     my_bar.progress(50, text='Medium Strength')
-            # elif len(password) > 3:
-            #     my_bar.progress(30, text='Weak Password')
-            # elif len(password) < 1:
-            #     my_bar.progress(0)
+                my_bar.progress(100, text=f'Very Strong Password! Score: {score}')
         progress_bar()
-        column1.header(password)
+        column1.header(st.session_state.password)
 
         def append_item(item):
             if len(st.session_state.passwords) == 5:
@@ -138,10 +136,14 @@ It ensures randomness for maximum security against hacking attempts🎉🚀.
                 st.session_state["passwords"].insert(0, item)
             else:
                 st.session_state["passwords"].insert(0, item)
+        def multi_func(item):
+            gen_pass()
+            append_item(item)
+            
 
-        butt2.button('🔁', on_click= append_item, args= [password])
+        butt2.button('🔁', on_click=multi_func, args= [st.session_state.password])
         with butt1:
-            st_copy_to_clipboard(password)
+            st_copy_to_clipboard(st.session_state.password)
 
 
     else:
@@ -151,6 +153,8 @@ It ensures randomness for maximum security against hacking attempts🎉🚀.
         st.subheader('Previous generated passwords:')
         for x in st.session_state.passwords:
             st.text(x)
+
+    st.header(st.session_state)
 
 if __name__ == "__main__":
     app()
